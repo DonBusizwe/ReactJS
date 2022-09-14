905dfc9fb93264aca3064d6d7f883d9cdeb7c253
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2022-09-14 09:47:36 +0200

    outta

---
+++ dynamic/src/App.js
@@ -0,0 +1,12 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DynamicTable from './DynamicTable';
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicTable />
+    </div>
+  );
+}
+
+export default App;
