667e917ebedf3cf010fed0c8fa8a05a5e47917e5
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2022-09-23 18:34:23 +0200

    gushshe

---
+++ BeginReactBook/chapter1/mlazi/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+ render() {
+ return (
+ <div>
+ <h1>
+ My First React App!
+ of the first DreamChase
+ </h1> 
+</div>
+ );
+ }
+}
+export default App;
