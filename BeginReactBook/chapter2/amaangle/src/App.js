be71d8473bb5da102ef6d1de469f12cc2f480ce2
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2022-09-23 18:38:30 +0200

    immabe

---
+++ BeginReactBook/chapter2/amaangle/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Products from './Products';
+
+
+
+
+class App extends Component {
+ render() {
+ return (
+ <div>
+ <h1>
+ My First React App!
+ of the first DreamChase v8</h1> 
+ <Products /> <Products /> <Products />
+</div>
+ );
+ }
+}
+export default App;
