f19a1e466ac0b153ee175c2e3c5778c3dc4bc9e9
Busizwe Ntsibande <Magnificentntsibande@gmail.com>
2022-09-26 21:19:21 +0200

    allround

---
+++ BeginReactBook/chapter5/gusheshe/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import JumboTronComponent from './JumboTronComponent';
+
+class App extends Component {
+ render() { 
+ return (
+ <div>
+ <JumboTronComponent body=' '/> 
+ </div>
+ );
+ }
+}
+export default App;
